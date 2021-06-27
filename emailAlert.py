0333bd01358d075244a1dcb6ad25efb870141b46
Ori Suchy <orisuchy@gmail.com>
2021-06-27 18:29:34 +0300

    Update emailAlert.py

--- emailAlert.py
+++ emailAlert.py
@@ -8,16 +8,18 @@ def email_alert(subject, body, to):
     msg['subject'] = subject
     msg['to'] = to
     msg['from'] = "Your loving husband"
+    # TODO: Put the email you would like to send the notifications here
+    # Need to use Google 2-step verification to get the password
     user = "YourEmail@gmail.com"
-    password = "YouPassword" #need to use Google 2-step verification
+    password = "YouPassword" 
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(user, password)
     server.send_message(msg)
     server.quit()
-
-
+    
+# TODO: Put the email you would like to get the test notifications here
 if __name__ == "__main__":
     email_alert("test subject", "test body", "YourTestMail@gmail.com")
 
